f8597566c12b963f74f7ec0353c020bf8d316d42
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-05-10 18:59:31 +0700

    feat: update

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+var app = express();
+const port = 3000;
+var server = app.listen(port, function () {
+  console.log("Listening on port " + port);
+});
+
+const io = require("socket.io")(server, {
+  allowEIO3: true,
+});
+
+app.use(express.static(path.join(__dirname, "")));
+var userConnections = [];
+io.on("connection", (socket) => {
+  console.log("Socket id is" + socket.id);
+  socket.on("userConnect", (data) => {
+    console.log(data);
+    var other_users = userConnections.filter(
+      (value) => value.meeting_id == data.meeting_id
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meeting_id,
+    });
+
+    other_users.forEach((value) => {
+      socket.to(value.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+  });
+
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
+});
